d115370ebdf9d9ab7ec009bbdb3581d63bbad06e
alkzDstudio <34172722+alkzDstudio@users.noreply.github.com>
2018-11-21 22:45:55 +0300

    Update autocorrelation.py

--- 1.4.1_1/autocorrelation.py
+++ 1.4.1_1/autocorrelation.py
@@ -1,27 +1,23 @@
-import numpy as np
 import cv2
+import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
+from mpl_toolkits.mplot3d import Axes3D
 
 from numpy import mean, var, std
 
-
-def average(numbers):
-    return float(sum(numbers)) / max(len(numbers), 1)
-
-
 def autocorrelation(obj_1, obj_2):
-    obj_1 = sum(obj_1)
-    obj_2 = sum(obj_2)
-    f1 = obj_1 - mean(obj_1)
-    f2 = obj_2 - mean(obj_2)
+    Y1 = obj_1[:,:,0]*0.299 + obj_1[:,:,1]*0.587 + obj_1[:,:,2]*0.114
+    Y2 = obj_2[:,:,0]*0.299 + obj_2[:,:,1]*0.587 + obj_2[:,:,2]*0.114
+    f1 = Y1 - mean(Y1)
+    f2 = Y2 - mean(Y2)
     Mobj = mean(f1*f2)
-    Sigma = std(obj_1)*std(obj_2)
+    Sigma = std(Y1)*std(Y2)
     rec = Mobj/Sigma
     return rec
 
-
-video = cv2.VideoCapture('../LR1_2.avi')
+video = cv2.VideoCapture('../LR1_3.avi')
 length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 list = []
@@ -29,26 +25,36 @@ list = []
 while(video.isOpened()):    
     ret, frame = video.read()
     if ret==True:	
-	list.append(frame) 
+        list.append(frame) 
     else:
         break
 
-nexList = np.zeros((length,length))
-tao = 1
+nexList = []
+tao = 0
 
-while (tao < length):
+for tao in range(length):
+    nexList.append([])
     i = 0
     while ((i + tao) < length):
-	nexList[tao][i] = (autocorrelation(list[i], list[i + tao]))
-	i = i + 1
+        nexList[tao].append(autocorrelation(list[i], list[i + tao]))
+        i = i + 1
     tao = tao + 1
 
-for i in range(3):
-   plt.figure(i + 1)
-   plt.plot(nexList[i + 1,:])
-   plt.show()
-
+hf = plt.figure()
+plt.plot(nexList[1])
+plt.ylim((-0.2,1.1))
+plt.title('video (LR1_3.avi): t = 1')
+plt.show()
+hf = plt.figure()
+plt.plot(nexList[4])
+plt.ylim((-0.2,1.1))
+plt.title('video (LR1_3.avi): t = 4')
+plt.show()
+hf = plt.figure()
+plt.plot(nexList[30])
+plt.title('video (LR1_3.avi): t = 30')
+plt.ylim((-0.2,1.1))
+plt.show()
 
 video.release()
 cv2.destroyAllWindows()
-
